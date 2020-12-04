87f74903c596c718af279d1d6d080bb8facfa492
Vmechanic <Raymondlochinhim@gmail.com>
2020-12-04 00:25:41 -0600

    Updated hangman.py code

--- Hangman.py
+++ Hangman.py
@@ -22,7 +22,73 @@ def findLetter(list,guess):
     for x in range(0, len(list)):
         if(list[x] == guess):
             letterIndices.append(x)
+    return letterIndicesword = "This is hangman".lower()
+
+wordIntoList = []
+guesses = []
+wrongGuesses = []
+countWrongGuesses = 0
+template = ""
+gameOver = False
+
+for x in word:
+    if x == " ":
+        template += " "
+    else:
+        template += "_"
+
+template = list(template)
+
+for x in word:
+    wordIntoList.append(x)
+
+
+def findLetter(list, guess):
+    letterIndices = []
+    for x in range(0, len(list)):
+        if(list[x] == guess):
+            letterIndices.append(x)
     return letterIndices
+
+
+def notifyPlayer():
+    print("Letters you guessed: )")
+    print(guesses)
+    print("\nLetters you guessed wrong: ")
+    if ((8 - countWrongGuesses) == 1):
+        print("You have 1 more guess left before you lose.")
+    else:
+        print("You have " + str(8 - countWrongGuesses) + " guesses left before you lose.")
+    print(wrongGuesses)
+    print("".join(template))
+
+
+print("".join(template) + "\n")
+print("There are " + str(len(word.replace(" ", ""))) +
+      " letters you have to guess (spaces excluded).\n")
+
+while gameOver == False:
+    guess = input("Guess a letter: ").lower()
+    if guess in guesses:
+        guess = input("You guessed this letter already. Pick another one: ")
+    if len(guess) > 1:
+        guess = input("You can't guess multiple letters try again: ")
+    guesses.append(guess)
+    if guess in word:
+        for x in findLetter(wordIntoList, guess):
+            template[x] = guess
+        notifyPlayer()
+    else:
+        wrongGuesses.append(guess)
+        countWrongGuesses += 1
+        notifyPlayer()
+    if (len(wrongGuesses) == 8):
+        gameOver = True
+        print("You guessed 8 incorrect letters. You lost!")
+    elif (word == "".join(template)):
+        gameOver = True
+        print("You win! You guessed all the correct letters!")
+
 
 def notifyPlayer():
   print("Letters you guessed: )")
